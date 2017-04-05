7e3f122271dda1a6ef38e96517acae63580fce89
wenzhida <zhida.wen@qishon.com>
2017-04-05 11:51:44 +0800

    中断线程案例

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 并发编程参考代码，陆陆续续的更新。。。
+
+### com.alabada.con01_Thread
+    Calculator01、Calculator02：
+        线程创建（实现Runnable接口的类，使用带参数的Thread构造器来创建Thread对象，这个参数就是实现Runnable接口的类的一个对象）。
+        线程基本状态获取。
+    PrimeGenerator:
+        继承Thread类，并且覆盖run()方法的方式来创建线程，并测试中断线程。
+
+
+
---
+++ src/main/java/com/alabada/con01_Thread/PrimeGenerator.java
@@ -0,0 +1,53 @@
+package com.alabada.con01_Thread;
+
+/**
+ * @Author 温枝达
+ * @Email alabadazi@gmail.com
+ * @Date 2017/4/5 11:29
+ * @Description 采用继承Thread的方式来创建线程
+ */
+public class PrimeGenerator extends Thread {
+
+    @Override
+    public void run() {
+        long number = 1L;
+        while (true) {
+            if (isPrime(number)) {
+                System.out.printf("Number %d is Prime", number);
+                System.out.println();
+            }
+
+            if (isInterrupted()) { // 获取该线程的状态：是否已被中断
+                System.out.printf("The Prime Generator has been Interruped");
+                System.out.println();
+                return;
+            }
+            number++;
+        }
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 2) {
+            return true;
+        }
+        for (long i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Thread task = new PrimeGenerator();
+        task.start(); // 开启线程
+
+        try {
+            Thread.sleep(5000); // 让main线程睡眠5秒，避免线程开启就关闭
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        task.interrupt(); // 中断线程
+    }
+
+}
