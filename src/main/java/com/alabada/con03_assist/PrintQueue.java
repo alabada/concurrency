e830d1be819990bc6fd1a29eb37b77089bc3da09
wenzhida <zhida.wen@qishon.com>
2017-04-06 17:50:21 +0800

    借助信号量对多个共享资源进行访问控制

--- src/main/java/com/alabada/con03_assist/PrintQueue.java
+++ src/main/java/com/alabada/con03_assist/PrintQueue.java
@@ -2,6 +2,8 @@ package com.alabada.con03_assist;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @Author 温枝达
@@ -13,10 +15,20 @@ public class PrintQueue {
 
     private final Semaphore semaphore;
 
+    private boolean freePrinters[];
+
+    private Lock lockPrinters;
+
     public PrintQueue() {
         // 将1作为传入参数，创建的就是二进制信号量。
-        // 信号量的内部计数器初始值是1，所以它只能保护一个共享资源的访问。
-        semaphore = new Semaphore(1);
+        // 如果信号量的内部计数器初始值是1，所以它只能保护一个共享资源的访问。
+        // 以下将信号量初始化为3
+        semaphore = new Semaphore(3);
+        freePrinters = new boolean[3];
+        for (int i = 0; i < 3; i++) {
+            freePrinters[i] = true;
+        }
+        lockPrinters = new ReentrantLock();
     }
 
     /**
@@ -31,16 +43,39 @@ public class PrintQueue {
         try {
             semaphore.acquire(); // 1、通过该方法获得信号量，
 
+            int assignedPrinter = getPrinter();
+
             // 2、
             Long duration = (long) (Math.random() * 10);
-            System.out.printf("%s: PrintQueue: Printing a Job during %d seconds\n", Thread.currentThread().getName(), duration);
-            Thread.sleep(duration);
-//            TimeUnit.SECONDS.sleep(duration);
+            System.out.printf("%s: PrintQueue: Printing a Job in Printer %d during %d seconds\n", Thread.currentThread().getName(), assignedPrinter, duration);
+            TimeUnit.SECONDS.sleep(duration);
+
+            freePrinters[assignedPrinter] = true;
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             semaphore.release(); // 3、释放信号量
         }
+    }
+
+    private int getPrinter() {
+        int ret = -1;
+
+        try {
+            lockPrinters.lock();
+            for (int i = 0; i < freePrinters.length; i++) {
+                if (freePrinters[i]) {
+                    ret = i;
+                    freePrinters[i] = false;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lockPrinters.unlock();
+        }
+        return ret;
     }
 
     public static void main(String args[]) {
